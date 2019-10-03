c257fe98f751cbdb88a46765936c95a25f9e73dd
Eric Lapouyade <elapouya@gmail.com>
2019-10-03 18:24:40 +0200

    generate minified js script

--- scripts/make_statics.sh
+++ scripts/make_statics.sh
@@ -11,15 +11,14 @@
 DJANGO_MODALS_DIR=$(dirname $0)/../django_modals
 CSSDIR=$DJANGO_MODALS_DIR/static/django_modals/css
 JSDIR=$DJANGO_MODALS_DIR/static/django_modals/js
-BETTERIMAGEDIR=~/projects/django-better-image/django_better_image
-BICSSDIR=$BETTERIMAGEDIR/static/django_modals/css
-BIJSDIR=$BETTERIMAGEDIR/static/django_modals/js
 
 DJANGO_MODALS_SCSS=$CSSDIR/djmodals.scss
 DJANGO_MODALS_CSS=$CSSDIR/djmodals.css
 DJANGO_MODALS_MIN_CSS=$CSSDIR/djmodals.min.css
-DJANGO_MODALS_JS=$JSDIR/djmodals_utils.js
-DJANGO_MODALS_MIN_JS=$JSDIR/djmodals_utils.min.js
+DJANGO_MODALS_JS=$JSDIR/djmodals.js
+DJANGO_MODALS_MIN_JS=$JSDIR/djmodals.min.js
+DJANGO_MODALS_UTILS_JS=$JSDIR/djmodals_utils.js
+DJANGO_MODALS_UTILS_MIN_JS=$JSDIR/djmodals_utils.min.js
 
 JS_COMPRESS="java -jar $HOME/bin/closure-compiler.jar --language_out=ECMASCRIPT_2015"
 YUI_COMPRESS=yui-compressor
@@ -35,10 +34,12 @@ fi
 
 if [[ -d $JSDIR ]]
 then
-    cp $JSDIR/util.js $DJANGO_MODALS_JS
-    echo >> $DJANGO_MODALS_JS
-    cat $JSDIR/modal.js >> $DJANGO_MODALS_JS
-    echo "generated : $DJANGO_MODALS_JS"
     $JS_COMPRESS $DJANGO_MODALS_JS > $DJANGO_MODALS_MIN_JS
     echo "generated : $DJANGO_MODALS_MIN_JS"
+    cp $JSDIR/util.js $DJANGO_MODALS_UTILS_JS
+    echo >> $DJANGO_MODALS_UTILS_JS
+    cat $JSDIR/modal.js >> $DJANGO_MODALS_UTILS_JS
+    echo "generated : $DJANGO_MODALS_UTILS_JS"
+    $JS_COMPRESS $DJANGO_MODALS_UTILS_JS > $DJANGO_MODALS_UTILS_MIN_JS
+    echo "generated : $DJANGO_MODALS_UTILS_MIN_JS"
 fi
